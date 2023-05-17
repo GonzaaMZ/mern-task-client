a9b37a02b12a3eaecb8bec9b9a92d342ad5e7d6d
GonzaaMZ <gonzaloamaya1999@gmail.com>
2023-05-17 16:13:10 -0300

    primera parte front terminada

--- src/components/projects/ListadoProyectos.jsx
+++ src/components/projects/ListadoProyectos.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
 import proyectoContext from "../../context/proyectos/proyectoContext";
 import Proyecto from "./Proyecto";
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
 
 const ListadoProyectos = () => {
 
@@ -9,6 +10,7 @@ const ListadoProyectos = () => {
     
     useEffect(() => {
         obtenerProyectos()
+        // eslint-disable-next-line
     }, [])
 
     if(proyectos.length === 0) return <p>No hay Proyectos</p>;
@@ -16,9 +18,13 @@ const ListadoProyectos = () => {
 
     return ( 
         <ul className="listado-proyectos">
+            <TransitionGroup>
             {proyectos.map((proyecto) => (
-                <Proyecto key={proyecto.id} proyecto={proyecto}/>
+               <CSSTransition key={proyecto.id} timeout={200} classNames="proyecto">
+                 <Proyecto proyecto={proyecto}/>
+               </CSSTransition>
             ))}
+            </TransitionGroup>
         </ul>
      );
 }
--- src/components/tareas/ListadoTareas.jsx
+++ src/components/tareas/ListadoTareas.jsx
@@ -1,4 +1,5 @@
 import { Fragment, useContext } from "react";
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import Tarea from "./Tarea";
 import proyectoContext from "../../context/proyectos/proyectoContext";
 import tareaContext from "../../context/tareas/tareaContext";
@@ -10,37 +11,45 @@ const ListadoTareas = () => {
     const proyectosContext = useContext(proyectoContext);
     const { proyecto, eliminarProyecto } = proyectosContext;
 
-     // obtener las tareas del proyecto
-     const tareasContext = useContext(tareaContext);
-     const {tareasproyecto} = tareasContext;
+    // obtener las tareas del proyecto
+    const tareasContext = useContext(tareaContext);
+    const { tareasproyecto } = tareasContext;
 
-    if(!proyecto) return <h2>Selecciona un Proyecto</h2>
+    if (!proyecto) return <h2>Selecciona un Proyecto</h2>
 
     // Array destructuring
-    const [ proyectoActual ] = proyecto;
+    const [proyectoActual] = proyecto;
 
     const onClickEliminar = () => {
         eliminarProyecto(proyectoActual.id)
     }
-    
-    return ( 
+
+    return (
         <Fragment>
             <h2>Proyecto: {proyectoActual.nombre}</h2>
             <ul className="listado-tareas">
                 {tareasproyecto.length === 0
-                ? (<li className="tarea"><p>No hay tareas</p></li>)
-                : tareasproyecto.map(tarea => (
-                    <Tarea key={tarea.nombre} tarea={tarea}/>
-                ))
+                    ? (<li className="tarea"><p>No hay tareas</p></li>)
+                    : <TransitionGroup>
+                    {tareasproyecto.map(tarea => (
+                       <CSSTransition 
+                       key={tarea.id}
+                       timeout={200}
+                       classNames="tarea"
+                       >
+                         <Tarea tarea={tarea} />
+                       </CSSTransition>
+                        ))}
+                    </TransitionGroup>
                 }
             </ul>
             <button
-            type="button"
-            className="btn btn-eliminar"
-            onClick={onClickEliminar}
+                type="button"
+                className="btn btn-eliminar"
+                onClick={onClickEliminar}
             >Eliminar Proyecto &times;</button>
         </Fragment>
-     );
+    );
 }
- 
+
 export default ListadoTareas;
